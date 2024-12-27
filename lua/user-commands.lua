3f030a499ab0eee9afdd988264eb9b7d01beb232
Ezechukwu Ojukwu <ezechukwu69@gmail.com>
2024-12-28 00:53:18 +0100

    Update to config

--- lua/user-commands.lua
+++ lua/user-commands.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_create_user_command("Instances", function(opts) end, {
 	nargs = 1,
-	complete = function(arg_lead, cmd_line, cursor_pos) end,
+	complete = function(arg_lead, cmd_line, cursor_pos) 
+    
+  end,
 })
